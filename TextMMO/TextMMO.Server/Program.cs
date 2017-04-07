600587f7324a12abd20a012a6fcfa0e64c6a68e8
Schneider, Lukas <L.Schneider@ptsgroup.de>
2017-04-07 14:32:34 +0200

    Re-commit - added simple server - client communication

--- TextMMO/TextMMO.Server/Program.cs
+++ TextMMO/TextMMO.Server/Program.cs
@@ -1,4 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
+using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,7 +13,7 @@ namespace TextMMO.Server
 {
     class Program
     {
-		/*
+        /*
 		
 		General ToDo:
 			
@@ -26,14 +28,49 @@ namespace TextMMO.Server
 			- retreive data
 			
 		*/
+        //static void Main(string[] args)
+        //{
+        //    Thread tickThread = new Thread(tick);
+        //    tickThread.Start(4500);
+        //    Thread.Sleep(1000);
+        //    Console.WriteLine("Main thread ({0}) exiting...",
+        //                Thread.CurrentThread.ManagedThreadId);
+        //}
+
         static void Main(string[] args)
         {
-            Thread tickThread = new Thread(tick);
-            tickThread.Start(4500);
-            Thread.Sleep(1000);
-            Console.WriteLine("Main thread ({0}) exiting...",
-                        Thread.CurrentThread.ManagedThreadId);
+            //Trigger the method PrintIncomingMessage when a packet of type 'Message' is received
+            //We expect the incoming object to be a string which we state explicitly by using <string>
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Message", PrintIncomingMessage);
+            //Start listening for incoming connections
+            Connection.StartListening(ConnectionType.TCP, new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0));
+
+            //Print out the IPs and ports we are now listening on
+            Console.WriteLine("Server listening for TCP connection on:");
+            foreach (System.Net.IPEndPoint localEndPoint in Connection.ExistingLocalListenEndPoints(ConnectionType.TCP))
+                Console.WriteLine("{0}:{1}", localEndPoint.Address, localEndPoint.Port);
+
+            //Let the user close the server
+            Console.WriteLine("\nPress any key to close server.");
+            Console.ReadKey(true);
+
+            //We have used NetworkComms so we should ensure that we correctly call shutdown
+            NetworkComms.Shutdown();
         }
+
+        /// <summary>
+        /// Writes the provided message to the console window
+        /// </summary>
+        /// <param name="header">The packet header associated with the incoming message</param>
+        /// <param name="connection">The connection used by the incoming message</param>
+        /// <param name="message">The message to be printed to the console</param>
+        private static void PrintIncomingMessage(PacketHeader header, Connection connection, string message)
+        {
+            Console.WriteLine("\nA message was received from " + connection.ToString() + " which said '" + message + "'.");
+        }
+
+
+
 
         static void tick(object obj)
         {
